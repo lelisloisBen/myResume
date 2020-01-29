2ad72eb1f2c5a76c46b7aea641ba6af98b18ad8c
samir benzada <samirbenzada@gmail.com>
2020-01-29 13:40:15 -0500

    -m

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -19,18 +19,56 @@ const Home = () => {
                 </div>
                 <div className="col-sm-12 col-md-9 col-lg-9">
                     <div className="tab-content" id="v-pills-tabContent">
+
                         <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
                             <img src={bg} alt="bg" width="80%" />
                         </div>
+
                         <div className="tab-pane fade" id="v-pills-project" role="tabpanel" aria-labelledby="v-pills-project-tab">
-                        The alien mothership is in orbit here. If we can hit that bullseye, the rest of the dominoes will fall like a house of cards. Checkmate.
+                            <div className="row">
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                                <div className="col-sm-12 col-md-4 col-lg-4">
+                                    <img src={bg} alt="test" width="100%" />
+                                    <p className="text-center">here the title</p>
+                                </div>
+                            </div>
                         </div>
+
                         <div className="tab-pane fade" id="v-pills-skills" role="tabpanel" aria-labelledby="v-pills-skills-tab">
                             Perhaps, but perhaps your civilization is merely the sewer of an even greater society above you! You guys realize you live in a sewer, right? My fellow Earthicans, as I have explained in my book 'Earth in the Balance'', and the much more popular ''Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards.
                         </div>
+
                         <div className="tab-pane fade" id="v-pills-about" role="tabpanel" aria-labelledby="v-pills-about-tab">
                         Just once I'd like to eat dinner with a celebrity who isn't bound and gagged. I haven't felt much of anything since my guinea pig died. And so we say goodbye to our beloved pet, Nibbler, who's gone to a place where I, too, hope one day to go. The toilet.
                         </div>
+
                         <div className="tab-pane fade" id="v-pills-contact" role="tabpanel" aria-labelledby="v-pills-contact-tab">
                        bla vla lcdlsca  Just once I'd like to eat dinner with a celebrity who isn't bound and gagged. I haven't felt much of anything since my guinea pig died. And so we say goodbye to our beloved pet, Nibbler, who's gone to a place where I, too, hope one day to go. The toilet.
                         </div>
